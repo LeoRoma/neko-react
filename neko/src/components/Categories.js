3af70af711cd5b76767cb3b60c1424c5635ae95a
LeoRoma <xiajtn@gmail.com>
2020-03-17 15:52:08 +0000

    displayed categories in a grid

--- neko/src/components/Categories.js
+++ neko/src/components/Categories.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import createTypography from '@material-ui/core/styles/createTypography';
+
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+
+
 
 class Categories extends Component {
   render() {
     const categories = this.props.categories
-    this.props.categories.map((category) => {
-      console.log(category.url)
-    })
-    console.log(this.props.categories)
+    console.log(categories)
     return (
-      <div>
-        {
-          categories.map((category) =>
-            <div>
-              <img
-                src={category.url}
-                width="620"
-                height="430">
-              </img>
-            </div>)
-        }
-
+      <div className>
+        <GridList cellHeight={260} cols={3}>
+          {categories.map(tile => (
+            <GridListTile key={tile.id}>
+              <img src={tile.url} />
+            </GridListTile>
+          ))}
+        </GridList>
       </div>
     )
   }
