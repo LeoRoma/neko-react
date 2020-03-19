ee329ee16fc96abb0a56dc1d239e249425116534
LeoRoma <xiajtn@gmail.com>
2020-03-19 16:25:07 +0000

    rows added in Categories container

---
+++ neko/src/components/CategoriesContainer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class CategoriesContainer extends Component{
+  render(){
+    return(
+      <div className="container">
+        <div className="row">
+          Dropdown
+        </div>
+        <div className="row">
+          cats
+        </div>
+      </div>
+    )
+  }
+}
